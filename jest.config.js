31577460f54324680e8edea47db04345cb2fce99
Ayrton Sato <ayrton_sato@hotmail.com>
2021-02-09 10:36:12 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
 	// Automatically clear mock calls and instances between every test
 	clearMocks: true,
 	// The directory where Jest should output its coverage files
@@ -6,7 +6,7 @@ export default {
 	// Indicates which provider should be used to instrument code for coverage
 	coverageProvider: 'v8',
 	// A list of reporter names that Jest uses when writing coverage reports
-	coverageReporters: ['<rootDir>/src/**/*.ts'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 	// The test environment that will be used for testing
 	testEnvironment: 'node',
 	// A map from regular expressions to paths to transformers
