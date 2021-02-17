878818fa4c178f5cd4fde79ade54a188f208940e
Ayrton Sato <ayrton_sato@hotmail.com>
2021-02-17 13:36:52 -0300

    refactor: remove duplicated code

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,5 +1,5 @@
 export * from '../../protocols/db/account/load-account-by-email-repository'
-export * from '../../protocols/db/update-access-token-repository'
+export * from '../../protocols/db/account/update-access-token-repository'
 export * from '../add-account/db-add-account-protocols'
 export * from './db-authentication'
 export * from '../../protocols/criptography/hash-comparer'
--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -8,13 +8,9 @@ import {
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helpers'
 
 export class LoginController implements Controller {
-	private readonly validation: Validation
-	private readonly authentication: Authentication
-
-	constructor (authentication: Authentication, validation: Validation) {
-		this.authentication = authentication
-		this.validation = validation
-	}
+	constructor (
+		private readonly authentication: Authentication,
+		private readonly validation: Validation) {}
 
 	async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
 		try {
