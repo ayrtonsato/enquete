b641a0dc0491a9bd01fab3a57f159759f9311614
Ayrton Sato <ayrton_sato@hotmail.com>
2021-02-22 18:31:01 -0300

    chore: enable debug

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
 	mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27018/clean-node-api',
-	port: process.env.PORT || 5000,
+	port: process.env.PORT || 5050,
 	jwtSecret: process.env.JWT_SECRET || '123XVCZasd12'
 }
