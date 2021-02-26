b3b347f8c4319b3f9e0b6fc460d31b75bf2b8351
Ayrton Sato <ayrton_sato@hotmail.com>
2021-02-26 11:37:23 -0300

    refactor: remove return await promise

--- src/data/usecases/add-survey/db-add-survey.ts
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -4,6 +4,5 @@ export class DbAddSurvey implements AddSurvey {
 	constructor (private readonly addSurveyRepository: AddSurveyRepository) {}
 	async add (data: AddSurveyModel): Promise<void> {
 		await this.addSurveyRepository.add(data)
-		return await new Promise(resolve => resolve())
 	}
 }
