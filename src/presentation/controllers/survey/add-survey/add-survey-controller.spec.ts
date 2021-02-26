8e625b59303763c6515152ea03825580d65ad3db
Ayrton Sato <ayrton_sato@hotmail.com>
2021-02-26 11:09:49 -0300

    test: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helpers'
+import { badRequest, serverError, noContent } from '../../../helpers/http/http-helpers'
 
 interface SutTypes {
 	sut: AddSurveyController
@@ -78,5 +78,11 @@ describe('Add Survey', () => {
 			.mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
 		const httpResponse = await sut.handle(makeFakeRequest())
 		expect(httpResponse).toEqual(serverError(new Error()))
+	})
+
+	test('Should returns 204 on success', async () => {
+		const { sut } = makeSut()
+		const httpResponse = await sut.handle(makeFakeRequest())
+		expect(httpResponse).toEqual(noContent())
 	})
 })
